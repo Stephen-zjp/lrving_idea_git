249fb6396a48a981e0f09c2d5bdf9906908e7209
zhaojiepeng <zhaojiepeng@163.com>
2019-12-05 19:40:44 +0800

    第一次提交

---
+++ src/main/java/demo/GitDemo.java
@@ -0,0 +1,9 @@
+package demo;
+
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello Git！");
+        System.out.println("123456");
+        System.out.println("789");
+    }
+}
